d22a28fa85791a19edd27a2a7e7f2a5e503d559c
Ruslan Akhmetshin <73955661+ruslanakhmett@users.noreply.github.com>
2022-08-24 16:01:48 +0300

    Update my_codewars_katas.py

--- my_codewars_katas.py
+++ my_codewars_katas.py
@@ -1,13 +1,68 @@
-'''Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+'''Write a function that takes in a string of one or more words, and returns the same string, 
+but with all five or more letter words reversed (Just like the name of this Kata). 
+Strings passed in will consist of only letters and spaces.
+Spaces will be included only when more than one word is present.
 
 Examples:
-
 spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
 spinWords( "This is a test") => returns "This is a test" 
 spinWords( "This is another test" )=> returns "This is rehtona test"'''
 
 def spin_words(sentence):
-    return ' '.join([word[::-1] if len(word) > 4  else word for word in sentence.split(' ')])
+    return ' '.join([word[::-1] if len(word) > 4 else word for word in sentence.split(' ')])
   
-  
-  ---------------------------------------------
+
+    
+
+----------------------------------------------
+'''Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2'''
+
+def digital_root(n):
+        return digital_root(sum([int(x) for x in str(n)])) if n > 9 else n
+
+    
+---------------------------------------------
+'''Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+Note: keep the original order of the names in the output.'''
+
+def friend(x):
+    return list(filter(lambda l: len(l) == 4, x))
+
+
+
+----------------------------------------------
+'''Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice'''
+
+def duplicate_count(text):
+    return len(set([char for char in text.lower() if text.lower().count(char) > 1]))
+
+
+----------------------------------------------
+
+
